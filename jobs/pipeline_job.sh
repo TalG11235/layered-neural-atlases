93b2bddf4287ed88d4da67a15d1241c704487f0e
Tal <talgorbunov@gmail.com>
2025-06-22 14:43:20 +0300

    fixed paths in pipeline job

--- jobs/pipeline_job.sh
+++ jobs/pipeline_job.sh
@@ -12,19 +12,18 @@ echo "Job started on $(hostname)"
 
 # general variables for the pipline
 PROJECT_DIR="$(pwd)"
-PROJECT_SRC_DIR=$PROJECT_DIR/src
-EXPERIMENTS_DIR=$PROJECT_DIR/experiments
-EXPERIMENTS_RESULTS_DIR=$EXPERIMENTS_DIR/results
-ATLASES_DIR=$EXPERIMENTS_DIR/atlases
-OUTPUT_DIR=$EXPERIMENTS_DIR/compressed_atlases
+PROJECT_SRC_DIR="$PROJECT_DIR/src"
+EXPERIMENTS_DIR="$PROJECT_DIR/experiments"
+EXPERIMENTS_RESULTS_DIR="$EXPERIMENTS_DIR/results"
+ATLASES_DIR="$EXPERIMENTS_DIR/atlases"
+OUTPUT_DIR="$EXPERIMENTS_DIR/compressed_atlases"
 RDEIC_DIR="$(pwd)/thirdparty/RDEIC"
-WEIGHT_DIR=$RDEIC_DIR/weight
+WEIGHT_DIR="$RDEIC_DIR/weight"
 CONFIG_PATH="$1"
-ATLASES_PROJECT_DIR="$(pwd)"
-RGB_ATLASES_DIR=$EXPERIMENTS_DIR/compressed_atlases
-ALPHA_ATLASES_OUTPUT_DIR=$EXPERIMENTS_DIR/compressed_atlases_alpha/
-CHECKPOINT_DIR=$EXPERIMENTS_DIR/checkpoint
-
+ATLAS_PROJECT_DIR="$(pwd)"
+RGB_ATLASES_DIR="$EXPERIMENTS_DIR/compressed_atlases"
+ALPHA_ATLASES_OUTPUT_DIR="$EXPERIMENTS_DIR/compressed_atlases_alpha"
+CHECKPOINT_DIR="$EXPERIMENTS_DIR/checkpoint"
 # create directory for experiments if doesn't exists
 if [ ! -d "$EXPERIMENTS_DIR" ]; then
   mkdir -p "$EXPERIMENTS_DIR"
@@ -34,13 +33,14 @@ if [ ! -d "$EXPERIMENTS_RESULTS_DIR" ]; then
 fi
 
 # clear previous intermidiate directories
-rm -rf $ATLASES_DIR
-rm -rf $OUTPUT_DIR
-rm -rf $CHECKPOINT_DIR
-rm -rf $RGB_ATLASES_DIR
-rm -rf $ALPHA_ATLASES_OUTPUT_DIR
+rm -rf "$ATLASES_DIR"
+rm -rf "$OUTPUT_DIR"
+rm -rf "$CHECKPOINT_DIR"
+rm -rf "$RGB_ATLASES_DIR"
+rm -rf "$ALPHA_ATLASES_OUTPUT_DIR"
 
 ##################################### Training ################################################
+echo "Training"
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate neural_atlases
 
@@ -49,16 +49,16 @@ python $PROJECT_SRC_DIR/train.py $CONFIG_PATH
 # Navigate into the latest directory inside results/
 exp_dir=$(find results/ -mindepth 1 -maxdepth 1 -type d | sort -V | tail -n 1)
 mv $exp_dir $EXPERIMENTS_RESULTS_DIR
+exp_dir_name=$(basename "$exp_dir")
 rm -rf results
 
 ################################ Atlases Compression ##########################################
 # Find the subdirectory with the highest number (frame index)
-latest_experiment=$(find "$EXPERIMENTS_RESULTS_DIR" -mindepth 1 -maxdepth 1 -type d | sort -V | tail -n 1)
+latest_experiment="$EXPERIMENTS_RESULTS_DIR/$(basename "$exp_dir")"
 latest_experiment_evaluation=$(find "$latest_experiment" -mindepth 1 -maxdepth 1 -type d | sort -V | tail -n 1)
-echo $latest_experiment_evaluation
 
 # Make sure the output folder exists
-mkdir -p $ATLASES_DIR
+mkdir -p "$ATLASES_DIR"
 # Copy the two texture files to atlases/
 cp "$latest_experiment_evaluation/texture_orig1.png" "$latest_experiment_evaluation/texture_orig2.png" $ATLASES_DIR
 
@@ -80,13 +80,11 @@ python3 $RDEIC_DIR/inference_partition.py \
 cd $PROJECT_DIR
 
 ##################################### Checkpoint Reduction ################################################
+echo "Checkpoint Reduction"
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate neural_atlases
 
-mkdir -p $CHECKPOINT_DIR
-
-echo $CHECKPOINT_DIR####################################
-echo $latest_experiment/checkpoint####################################
+mkdir -p "$CHECKPOINT_DIR"
 
 python <<EOF
 import torch
@@ -104,6 +102,7 @@ torch.save(checkpoint, "$CHECKPOINT_DIR/checkpoint")
 EOF
 
 ##################################### Atlases Alpha Layers ################################################
+echo "Atlases Alpha Layers"
 # add alpha channels to atlases
 source ~/miniconda3/etc/profile.d/conda.sh
 
@@ -134,16 +133,15 @@ imageio.imwrite("$output_file", (rgba * 255).astype(np.uint8))
 EOF
 
 done
-
-mkdir $EXPERIMENTS_DIR/outputs/
-final_output_folder=$EXPERIMENTS_DIR/outputs/$(basename "$exp_dir")
+mkdir "$EXPERIMENTS_DIR/outputs/"
+final_output_folder="$EXPERIMENTS_DIR/outputs/$exp_dir_name"
 # copy everything to one folder
-mkdir -p $final_output_folder
+mkdir -p "$final_output_folder"
 
 cp $CONFIG_PATH $ALPHA_ATLASES_OUTPUT_DIR/texture_orig1.png $ALPHA_ATLASES_OUTPUT_DIR/texture_orig2.png $final_output_folder
 
 ##################################### Checkpoint Quiantization ################################################
-
+echo "Checkpoint Quiantization"
 # quantization
 source ~/miniconda3/etc/profile.d/conda.sh
 
@@ -152,6 +150,7 @@ conda activate neural_atlases
 python $PROJECT_SRC_DIR/quantize_checkpoint.py $CONFIG_PATH $CHECKPOINT_DIR/checkpoint $final_output_folder/checkpoint
 
 ##################################### Video Reconstruction ################################################
+echo "Video Reconstruction"
 # recounstruct video with compressed atlases
 python $PROJECT_SRC_DIR/only_edit.py --trained_model_folder=$final_output_folder/ \
 --video_name=blackswan \
